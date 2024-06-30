93d1810a1fa6164faf654d1c37cae15e28676125
nathan33 <lee1928@outlook.com>
2024-06-30 22:28:16 +0800

    变更后端地址

--- src/stores/use-user-token/axios.ts
+++ src/stores/use-user-token/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const $axios = axios.create({
-    baseURL: 'http://8.222.190.33:3000/api',
+    baseURL: 'https://nathan33.xyz:3000/api',
     headers: {
         'Content-Type': 'application/json'
     }
