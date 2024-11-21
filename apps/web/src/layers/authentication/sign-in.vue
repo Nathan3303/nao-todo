76e0e9db28cd7530e8bfb774ea22a02aa3c093a0
Nathan Lee <67311306+Nathan3303@users.noreply.github.com>
2024-11-21 14:32:18 +0800

    [1] 调整认证界面布局

--- apps/web/src/layers/authentication/sign-in.vue
+++ apps/web/src/layers/authentication/sign-in.vue
@@ -1,41 +1,44 @@
 <template>
     <nue-div
-        vertical
         align="stretch"
-        justify="center"
         flex
-        width="320px"
         gap="24px"
+        justify="center"
+        vertical
+        width="320px"
         @keydown.enter="handleSignIn"
     >
-        <nue-div vertical gap="4px" align="center">
+        <nue-div align="center" gap="4px" vertical>
             <nue-text size="24px" weight="bold">登录到 NaoTodo</nue-text>
-            <nue-text size="12px" color="grey">使用您的电子邮件和密码进行登录</nue-text>
+            <nue-text color="grey" size="12px">使用您的电子邮件和密码进行登录</nue-text>
         </nue-div>
-        <nue-div vertical align="stretch">
+        <nue-div align="stretch" vertical>
             <nue-input
                 v-model="email"
+                :disabled="loading"
                 placeholder="电子邮箱 (name@example.com)"
                 type="email"
-                :disabled="loading"
             />
             <nue-input
                 v-model="password"
-                placeholder="密码"
-                type="password"
                 :disabled="loading"
                 allow-show-password
+                placeholder="密码"
+                type="password"
             />
-            <nue-button theme="primary" @click="handleSignIn" :loading="loading">登录</nue-button>
+            <nue-button :loading="loading" theme="primary" @click="handleSignIn">登录</nue-button>
         </nue-div>
-        <nue-divider align="center">
-            <nue-text color="grey" size="12px">或以其他方式登录</nue-text>
-        </nue-divider>
-        <nue-button icon="logo">NueUI</nue-button>
+        <!--        <nue-divider align="center">-->
+        <!--            <nue-text color="grey" size="12px">或以其他方式登录</nue-text>-->
+        <!--        </nue-divider>-->
+        <!--        <nue-button icon="logo">NueUI</nue-button>-->
+        <nue-text align="center" color="gray" size="12px">
+            点击登录按钮后，您可以在接下来的 30 天内免密访问您的账户，直到您退出或在别处登录。
+        </nue-text>
     </nue-div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
 import { ref } from 'vue'
 import { NueMessage } from 'nue-ui'
 import type { SigninOptions } from '@nao-todo/types'
--- apps/web/src/layers/authentication/sign-up.vue
+++ apps/web/src/layers/authentication/sign-up.vue
@@ -1,48 +1,48 @@
 <template>
     <nue-div
-        vertical
         align="stretch"
-        justify="center"
         flex
-        width="320px"
         gap="24px"
+        justify="center"
+        vertical
+        width="320px"
         @keydown.enter="handleSignUp"
     >
-        <nue-div vertical gap="4px" align="center">
+        <nue-div align="center" gap="4px" vertical>
             <nue-text size="24px" weight="bold">创建您的账户</nue-text>
-            <nue-text size="12px" color="grey" align="center">
+            <nue-text align="center" color="grey" size="12px">
                 在下面输入您的电子邮件和密码以创建账户
             </nue-text>
         </nue-div>
-        <nue-div vertical align="stretch">
+        <nue-div align="stretch" vertical>
             <nue-input
                 v-model="email"
+                :disabled="loading"
                 placeholder="电子邮箱 (name@example.com)"
                 type="email"
-                :disabled="loading"
             />
             <nue-input
                 v-model="password"
-                placeholder="密码"
-                type="password"
                 :disabled="loading"
                 allow-show-password
+                placeholder="密码"
+                type="password"
             />
             <nue-input
                 v-model="passwordConfirm"
-                placeholder="确认密码"
-                type="password"
                 :disabled="loading"
                 allow-show-password
+                placeholder="确认密码"
+                type="password"
             />
-            <nue-input v-model="nickname" placeholder="昵称（可选）" :disabled="loading" />
-            <nue-button theme="primary" @click="handleSignUp" :loading="loading">注册</nue-button>
+            <nue-input v-model="nickname" :disabled="loading" placeholder="昵称（可选）" />
+            <nue-button :loading="loading" theme="primary" @click="handleSignUp">注册</nue-button>
         </nue-div>
-        <nue-divider align="center">
-            <nue-text color="grey" size="12px">或以其他方式注册</nue-text>
-        </nue-divider>
-        <nue-button icon="logo">NueUI</nue-button>
-        <nue-text size="12px" color="gray" align="center">
+        <!--        <nue-divider align="center">-->
+        <!--            <nue-text color="grey" size="12px">或以其他方式注册</nue-text>-->
+        <!--        </nue-divider>-->
+        <!--        <nue-button icon="logo">NueUI</nue-button>-->
+        <nue-text align="center" color="gray" size="12px">
             点击注册按钮后，即表示您同意我们站点的
             <nue-link>服务条款</nue-link>
             和
@@ -52,7 +52,7 @@
     </nue-div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
 import { ref } from 'vue'
 import { NueMessage } from 'nue-ui'
 import type { SignupOptions } from '@nao-todo/types'
