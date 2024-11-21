e47d2fe9359a5ea63442151187b099e16ae92d2b
Nathan Lee <67311306+Nathan3303@users.noreply.github.com>
2024-11-21 19:24:31 +0800

    [1] 修复若干问题

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,38 +3,27 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import terser from '@rollup/plugin-terser'
 
-const isProd = process.env.NODE_ENV === 'production'
-const isDev = process.env.NODE_ENV === 'development'
-const isTest = process.env.NODE_ENV === 'test'
-
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
         terser({
             compress: {
-                sequences: isProd,
-                arguments: isProd,
-                drop_console: isProd && ['log'],
-                drop_debugger: isProd,
-                passes: isProd ? 4 : 1,
-                global_defs: {
-                    '@PROD': JSON.stringify(isProd),
-                    '@DEV': JSON.stringify(isDev),
-                    '@TEST': JSON.stringify(isTest)
-                }
+                sequences: true,
+                arguments: true,
+                drop_console: ['log'],
+                drop_debugger: true,
+                passes: 4
             },
             format: {
                 semicolons: false,
-                shorthand: isProd,
-                braces: !isProd,
-                comments: !isProd
+                shorthand: true,
+                braces: false,
+                comments: false
             },
             mangle: {
-                toplevel: isProd,
-                eval: isProd,
-                keep_classnames: isDev,
-                keep_fnames: isDev
+                toplevel: true,
+                eval: true
             }
         })
     ],
