cf60ea09c2c3b84937b6043f6e2cc3bc9948f650
Nathan Lee <lee1928@outlook.com>
2024-12-24 22:06:38 +0800

    [patch] 导出 “列表视图” 中的表格组件

--- apps/web/src/layers/todo-view-table/index.ts
+++ apps/web/src/layers/todo-view-table/index.ts
@@ -1,5 +1,7 @@
 import Table from './table.vue'
+import TableComp from './table/table.vue'
 import ColumnSelector from './column-selector/column-selector.vue'
 
+export const TodoTable = TableComp
 export const TodoViewTable = Table
 export const TodoTableColumnSelector = ColumnSelector
