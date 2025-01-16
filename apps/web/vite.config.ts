c739168298522b582d6f295f2f7f541bfaa773bd
Nathan Lee <67311306+Nathan3303@users.noreply.github.com>
2025-01-16 20:13:56 +0800

    [chore] 优化构建选项

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -36,6 +36,24 @@ export default defineConfig({
     },
     build: {
         outDir: 'dist',
-        minify: false
+        minify: false,
+        cssCodeSplit: true,
+        rollupOptions: {
+            output: {
+                assetFileNames: (assetInfo) => {
+                    if (assetInfo.type === 'asset' && (assetInfo.name as string).endsWith('.css'))
+                        return 'css/[name].[hash].[ext]'
+                    return assetInfo.name as string
+                },
+                chunkFileNames: 'js/[name].[hash].js',
+                manualChunks: (path) => {
+                    if (path.includes('node_modules')) return 'vendor'
+                    else if (path.includes('/apps/web/src/themes')) return 'theme'
+                    else if (path.includes('/apps/web/src/routes')) return 'routes'
+                    else if (path.includes('/apps/web/src/stores')) return 'stores'
+                    return void 0
+                }
+            }
+        }
     }
 })
