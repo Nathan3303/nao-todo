92f0eec0cb2257f3877c2dc26d3364396859e5e3
Nathan Lee <Lee1928@outlook.com>
2025-02-12 21:05:00 +0800

    [feat&patch] 新增 AI 对话界面以及修复任务界面的若干问题

--- apps/web/src/views/index/index.vue
+++ apps/web/src/views/index/index.vue
@@ -1,6 +1,9 @@
 <template>
     <nue-container class="index-view" theme="horizontal">
-        <nue-header v-if="viewStore.indexHeaderVisible" class="index-view__header">
+        <nue-header
+            v-if="viewStore.indexHeaderVisible"
+            class="index-view__header"
+        >
             <template #logo>
                 <user-dropdown
                     :user="user"
@@ -12,21 +15,60 @@
             </template>
             <template #navigators>
                 <nue-div vertical gap="24px">
-                    <nue-tooltip content="任务" placement="right-center" size="small">
+                    <nue-tooltip
+                        content="任务"
+                        placement="right-center"
+                        size="small"
+                    >
                         <nue-link
                             icon="square-check-fill"
                             route="/tasks"
                             theme="index-header-link"
                         />
                     </nue-tooltip>
-                    <nue-tooltip content="日历视图" placement="right-center" size="small">
-                        <nue-link icon="calendar" route="/calendar" theme="index-header-link" />
+                    <nue-tooltip
+                        content="日历视图"
+                        placement="right-center"
+                        size="small"
+                    >
+                        <nue-link
+                            icon="calendar"
+                            route="/calendar"
+                            theme="index-header-link"
+                        />
                     </nue-tooltip>
-                    <nue-tooltip content="番茄专注" placement="right-center" size="small">
-                        <nue-link icon="focus2" route="/fqfocus" theme="index-header-link" />
+                    <nue-tooltip
+                        content="番茄专注"
+                        placement="right-center"
+                        size="small"
+                    >
+                        <nue-link
+                            icon="focus2"
+                            route="/fqfocus"
+                            theme="index-header-link"
+                        />
                     </nue-tooltip>
-                    <nue-tooltip content="搜索" placement="right-center" size="small">
-                        <nue-link icon="search2" route="/search" theme="index-header-link" />
+                    <nue-tooltip
+                        content="搜索"
+                        placement="right-center"
+                        size="small"
+                    >
+                        <nue-link
+                            icon="search2"
+                            route="/search"
+                            theme="index-header-link"
+                        />
+                    </nue-tooltip>
+                    <nue-tooltip
+                        content="对话大模型"
+                        placement="right-center"
+                        size="small"
+                    >
+                        <nue-link
+                            icon="chat"
+                            route="/ai"
+                            theme="index-header-link"
+                        />
                     </nue-tooltip>
                 </nue-div>
             </template>
@@ -58,7 +100,10 @@ import { UpdatePasswordDialog, IndexAside } from '@/layers'
 import { useUserStore, useViewStore } from '@/stores'
 import { UserDropdown } from '@nao-todo/components'
 import { UserProfileDialog } from '@/layers/user-profile-dialog'
-import { type IndexViewCtx, IndexViewCtxKey } from '@nao-todo/types/views/index-view'
+import {
+    type IndexViewCtx,
+    IndexViewCtxKey
+} from '@nao-todo/types/views/index-view'
 
 const userStore = useUserStore()
 const viewStore = useViewStore()
