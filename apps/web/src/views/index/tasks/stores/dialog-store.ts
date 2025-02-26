c6c2a3f29bdf2b4c9b993f59bf67cde79dbf34fb
Nathan Lee <Lee1928@outlook.com>
2025-02-27 01:30:14 +0800

    [patch] 修复在标签管理器中无法修改标签颜色的问题

--- apps/web/src/views/index/tasks/stores/dialog-store.ts
+++ apps/web/src/views/index/tasks/stores/dialog-store.ts
@@ -1,10 +1,15 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
-import { CreateProjectDialog, CreateTagDialog, CreateTodoDialog } from '@/layers'
+import {
+    CreateProjectDialog,
+    CreateTagDialog,
+    CreateTodoDialog
+} from '@/layers'
 import { useProjectStore, useTagStore, useUserStore } from '@/stores'
 import { useTasksViewStore } from './view-store'
 import { TagColorSelectDialog } from '@nao-todo/components'
 import type { NaoDialogManager } from '@/layouts/dialog-manager'
+import type { Tag } from '@nao-todo/types'
 
 export const useTasksDialogStore = defineStore('TasksDialogStore', () => {
     const projectStore = useProjectStore()
@@ -12,10 +17,12 @@ export const useTasksDialogStore = defineStore('TasksDialogStore', () => {
     const userStore = useUserStore()
     const tasksViewStore = useTasksViewStore()
 
-    const createProjectDialogRef = ref<InstanceType<typeof CreateProjectDialog>>() // 创建清单对话框
+    const createProjectDialogRef =
+        ref<InstanceType<typeof CreateProjectDialog>>() // 创建清单对话框
     const createTodoDialogRef = ref<InstanceType<typeof CreateTodoDialog>>() // 创建待办对话框
     const createTagDialogRef = ref<InstanceType<typeof CreateTagDialog>>() // 创建标签对话框
-    const tagColorSelectDialogRef = ref<InstanceType<typeof TagColorSelectDialog>>() // 标签颜色选择对话框
+    const tagColorSelectDialogRef =
+        ref<InstanceType<typeof TagColorSelectDialog>>() // 标签颜色选择对话框
     const dialogManagerRef = ref<InstanceType<typeof NaoDialogManager>>()
 
     const dialogManagerShow = async (dialogId: string) => {
@@ -54,12 +61,21 @@ export const useTasksDialogStore = defineStore('TasksDialogStore', () => {
     }
 
     // 显示标签颜色选择对话框
-    const showTagColorSelectDialog = async () => {
-        if (!tasksViewStore.viewInfo) return
-        tagColorSelectDialogRef.value?.show(
-            tasksViewStore.viewInfo.id,
-            (tasksViewStore.viewInfo.payload?.color as string) || 'transparent'
-        )
+    const showTagColorSelectDialog = async (tagId?: Tag['id'] | PointerEvent) => {
+        let _tagId, _tagColor
+        if (tagId && typeof tagId === 'string') {
+            const tag = tagStore.getTagByIdFromLocal(tagId)
+            _tagId = tagId
+            _tagColor = tag?.color ?? 'transparent'
+        } else {
+            if (!tasksViewStore.viewInfo) return
+            _tagId = tasksViewStore.viewInfo.id
+            _tagColor =
+                (tasksViewStore.viewInfo.payload?.color as string) ||
+                'transparent'
+        }
+        console.log(_tagId, _tagColor);
+        tagColorSelectDialogRef.value?.show(_tagId, _tagColor)
     }
 
     return {
