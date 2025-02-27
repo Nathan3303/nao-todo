dfb40d4cbef5231a20a1b3f7dad79246a0dfd836
Nathan Lee <Lee1928@outlook.com>
2025-02-27 20:59:42 +0800

    [feat] 实现任务板块中双侧栏宽度数据的持久化

--- apps/web/src/views/index/tasks/index.vue
+++ apps/web/src/views/index/tasks/index.vue
@@ -7,28 +7,42 @@
             :allow-resize-aside="responsiveFlag > 1"
             aside-max-width="300px"
             aside-min-width="200px"
-            aside-width="250px"
+            @after-aside-resize="tasksLayoutStore.handleAfterAsideResize"
+            :aside-width="asideWidth"
             class="tasks-view__main"
             outline-max-width="480px"
             outline-min-width="360px"
-            outline-width="36%"
+            :outline-width="outlineWidth"
+            @after-outline-resize="tasksLayoutStore.handleAfterOutlineResize"
         >
             <template v-if="projectAsideVisible" #aside>
-                <aside-link icon="more2" route-name="tasks-all">所有</aside-link>
+                <aside-link icon="more2" route-name="tasks-all">
+                    所有
+                </aside-link>
                 <aside-link icon="calendar2" route-name="tasks-today">
                     今天 - {{ now.format('MM月DD日, dddd') }}
                 </aside-link>
-                <aside-link icon="tomorrow2" route-name="tasks-tomorrow">明天</aside-link>
-                <aside-link icon="week3" route-name="tasks-week">本周</aside-link>
-                <aside-link icon="inbox" route-name="tasks-inbox">收集箱</aside-link>
+                <aside-link icon="tomorrow2" route-name="tasks-tomorrow">
+                    明天
+                </aside-link>
+                <aside-link icon="week3" route-name="tasks-week">
+                    本周
+                </aside-link>
+                <aside-link icon="inbox" route-name="tasks-inbox">
+                    收集箱
+                </aside-link>
                 <nue-divider />
                 <nue-collapse v-model="collapseItemsRecord">
                     <project-smart-list />
                     <tag-smart-list />
                 </nue-collapse>
                 <nue-divider />
-                <aside-link icon="heart" route-name="tasks-favorite">已收藏</aside-link>
-                <aside-link icon="delete" route-name="tasks-recycle">垃圾桶</aside-link>
+                <aside-link icon="heart" route-name="tasks-favorite">
+                    已收藏
+                </aside-link>
+                <aside-link icon="delete" route-name="tasks-recycle">
+                    垃圾桶
+                </aside-link>
             </template>
             <template #content>
                 <router-view />
@@ -36,7 +50,9 @@
             <template v-if="tasksOutlineVisible" #outline>
                 <todo-multi-details
                     v-if="tasksViewStore.multiSelectStates.isShowMultiDetails"
-                    :selected-ids="tasksViewStore.multiSelectStates.selectedTodoIds"
+                    :selected-ids="
+                        tasksViewStore.multiSelectStates.selectedTodoIds
+                    "
                 />
                 <todo-details-v2 v-else />
             </template>
@@ -59,8 +75,14 @@
         ref="createProjectDialogRef"
         :handler="tasksHandlerStore.handleCreateProject"
     />
-    <create-todo-dialog ref="createTodoDialogRef" :handler="tasksHandlerStore.handleCreateTodo" />
-    <create-tag-dialog ref="createTagDialogRef" :handler="tasksHandlerStore.handleCreateTag" />
+    <create-todo-dialog
+        ref="createTodoDialogRef"
+        :handler="tasksHandlerStore.handleCreateTodo"
+    />
+    <create-tag-dialog
+        ref="createTagDialogRef"
+        :handler="tasksHandlerStore.handleCreateTag"
+    />
     <tag-color-select-dialog
         ref="tagColorSelectDialogRef"
         :handler="tasksHandlerStore.handleSelectTagColor"
@@ -74,7 +96,12 @@ import { storeToRefs } from 'pinia'
 import { useViewStore } from '@/stores'
 import { AsideLink, TagColorSelectDialog } from '@nao-todo/components'
 import { useMoment } from '@nao-todo/utils'
-import { useTasksDialogStore, useTasksHandlerStore, useTasksViewStore } from '.'
+import {
+    useTasksDialogStore,
+    useTasksHandlerStore,
+    useTasksLayoutStore,
+    useTasksViewStore
+} from '.'
 import { NaoDialogManager } from '@/layouts/dialog-manager'
 import {
     CreateProjectDialog,
@@ -91,9 +118,12 @@ const viewStore = useViewStore()
 const tasksViewStore = useTasksViewStore()
 const tasksDialogStore = useTasksDialogStore()
 const tasksHandlerStore = useTasksHandlerStore()
+const tasksLayoutStore = useTasksLayoutStore()
 
 const now = useMoment()
-const { projectAsideVisible, responsiveFlag, tasksOutlineVisible } = storeToRefs(viewStore)
+const { projectAsideVisible, responsiveFlag, tasksOutlineVisible } =
+    storeToRefs(viewStore)
+const { asideWidth, outlineWidth } = storeToRefs(tasksLayoutStore)
 const createProjectDialogRef = ref<InstanceType<typeof CreateProjectDialog>>()
 const createTodoDialogRef = ref<InstanceType<typeof CreateTodoDialog>>()
 const createTagDialogRef = ref<InstanceType<typeof CreateTagDialog>>()
--- apps/web/src/views/index/tasks/stores/index.ts
+++ apps/web/src/views/index/tasks/stores/index.ts
@@ -1,3 +1,4 @@
 export * from './view-store'
 export * from './dialog-store'
 export * from './handler-store'
+export * from './layout-store'
