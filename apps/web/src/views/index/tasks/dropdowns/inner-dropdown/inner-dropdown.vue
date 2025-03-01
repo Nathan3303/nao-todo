576214baccc22547d86d15b42233d6dcef032348
Nathan Lee <Lee1928@outlook.com>
2025-03-01 15:09:07 +0800

    [patch] 修复筛选与排序菜单中多个下拉列表能同时展开的问题

--- apps/web/src/views/index/tasks/dropdowns/inner-dropdown/inner-dropdown.vue
+++ apps/web/src/views/index/tasks/dropdowns/inner-dropdown/inner-dropdown.vue
@@ -1,20 +1,3 @@
-<script setup lang="ts">
-import type { InnerDropdownEmits, InnerDropdownProps } from './types'
-
-withDefaults(defineProps<InnerDropdownProps>(), {
-    title: '选项标题',
-    icon: 'plus-circle',
-    hideOnClicked: false,
-    suffix: 0,
-    disabled: false
-})
-const emit = defineEmits<InnerDropdownEmits>()
-
-const handleDropdownExecute = (executeId: string) => {
-    emit('execute', executeId)
-}
-</script>
-
 <template>
     <nue-dropdown
         nesting
@@ -23,31 +6,63 @@ const handleDropdownExecute = (executeId: string) => {
         @execute="handleDropdownExecute"
         size="small"
         :disabled="disabled"
+        ref="InnerDropdownRef"
     >
-        <template #default="{ clickTrigger }">
-            <li
-                class="nue-dropdown-item"
-                :data-disabled="disabled"
-                @click.stop="clickTrigger"
-            >
-                <nue-icon :name="icon" />
-                {{ title }}
-                <nue-text color="orange" style="margin-left: auto" size="12px">
-                    <template v-if="suffix">
-                        <template v-if="typeof suffix === 'number'">
-                            {{ suffix }}
-                        </template>
-                        <template v-else> !</template>
+        <li
+            class="nue-dropdown-item"
+            :data-disabled="disabled"
+            @click="handleToggleDropdown"
+        >
+            <nue-icon :name="icon" />
+            {{ title }}
+            <nue-text color="orange" style="margin-left: auto" size="12px">
+                <template v-if="suffix">
+                    <template v-if="typeof suffix === 'number'">
+                        {{ suffix }}
                     </template>
-                </nue-text>
-                <nue-icon name="arrow-right" />
-            </li>
-        </template>
+                    <template v-else> !</template>
+                </template>
+            </nue-text>
+            <nue-icon name="arrow-right" />
+        </li>
         <template #dropdown>
             <slot></slot>
         </template>
     </nue-dropdown>
 </template>
+
+<script setup lang="ts">
+import { onBeforeUnmount, onMounted, ref } from 'vue'
+import useDropdownController from './use-dropdown-controller'
+import { NueDropdown } from 'nue-ui'
+import type { InnerDropdownEmits, InnerDropdownProps } from './types'
+
+const props = withDefaults(defineProps<InnerDropdownProps>(), {
+    title: '选项标题',
+    icon: 'plus-circle',
+    hideOnClicked: false,
+    suffix: 0,
+    disabled: false,
+    groupName: 'default'
+})
+const emit = defineEmits<InnerDropdownEmits>()
+
+const { bind, unBind, hideOthers } = useDropdownController()
+const InnerDropdownRef = ref<InstanceType<typeof NueDropdown>>()
+
+const handleDropdownExecute = (executeId: string) => {
+    emit('execute', executeId)
+}
+
+const handleToggleDropdown = () => {
+    if (!InnerDropdownRef.value) return
+    if (!InnerDropdownRef.value.visible) hideOthers(props.groupName)
+    InnerDropdownRef.value.toggle()
+}
+
+onMounted(() => bind(props.groupName, InnerDropdownRef))
+onBeforeUnmount(() => unBind(props.groupName, InnerDropdownRef))
+</script>
 
 <style scoped>
 .nue-dropdown-item[data-disabled='true'] {
