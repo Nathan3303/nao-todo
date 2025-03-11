3ae01986e41957126b951f91670aad4a59e8966f
Nathan Lee <Lee1928@outlook.com>
2025-03-12 00:55:04 +0800

    [patch] 优化 TasksDetails 内部组件导出

--- apps/web/src/views/index/tasks/components/outlines/details/index.ts
+++ apps/web/src/views/index/tasks/components/outlines/details/index.ts
@@ -1,15 +1,28 @@
 import { defineAsyncComponent } from 'vue'
 import DetailsEmpty from './details-empty.vue'
-import DetailsHeader from './details-header.vue'
 import DetailsLoading from './details-loading.vue'
 import DetailsRow from './details-row.vue'
+import DetailsHeader from './details-header.vue'
 import DetailsMain from './details-main.vue'
+import DetailsMainComments from './details-main-comments.vue'
+import DetailsMainEvents from './details-main-events.vue'
 import DetailsFooter from './details-footer.vue'
 
-export { DetailsEmpty, DetailsHeader, DetailsLoading, DetailsRow, DetailsMain, DetailsFooter }
+export {
+    DetailsEmpty,
+    DetailsHeader,
+    DetailsLoading,
+    DetailsRow,
+    DetailsMain,
+    DetailsFooter,
+    DetailsMainComments,
+    DetailsMainEvents
+}
+
 export const TasksDetails = defineAsyncComponent({
     loader: () => import('./details.vue')
 })
+
 export const TasksFloatDetails = defineAsyncComponent({
     loader: () => import('./float-details.vue')
 })
