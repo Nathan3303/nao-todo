a41a2a74d536b395a825a39c3a4ce7552af08c1e
Natan3303 <lee1928@outlook.com>
2025-10-22 18:00:45 +0800

    refactor: 20251022 重构 #3

--- apps/web/src/layouts/tasks/main/project/index.vue
+++ apps/web/src/layouts/tasks/main/project/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, reactive, watchEffect } from 'vue'
+import { ref, reactive, watch } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { useTasksViewStore } from '@/stores/tasks'
 import { Loading as LoadingComp } from '@nao-todo/components'
@@ -16,34 +16,35 @@ const loading = ref(true)
 const error = reactive({ message: '', errorImage: '/images/error.png' })
 const { viewProps } = storeToRefs(tasksViewStore)
 
-// @effect 当路由信息变化重新加载数据
-watchEffect(() => {
-    // 判断 projectId 是否为空
-    if (!props.projectId) {
-        error.message = '参数错误'
-        return
-    }
-    // 加载视图数据
-    loading.value = true
-    tasksViewStore
-        .loadViewProps(props.projectId, route.meta.category as string)
-        .then(() => {
-            // 判断 viewProps 是否存在
-            if (!viewProps.value) {
-                error.message = '视图数据加载失败'
-                return
-            }
-            // 重置错误信息
-            error.message = ''
-            // 跳转至指定视图
-            if (props.todoId) return
-            router.replace({
-                name: 'tasks-project-main',
-                params: { viewType: viewProps.value.preference.viewType }
-            })
+// @watch 监听路由信息变化，当在 id 改变时重新加载数据
+watch(
+    () => props.projectId,
+    async (newId) => {
+        // 判断 projectId 是否为空
+        if (!newId) {
+            error.message = '参数错误'
+            return
+        }
+        // 加载视图数据
+        loading.value = true
+        await tasksViewStore.loadViewProps(newId, route.meta.category as string)
+        loading.value = false
+        // 判断 viewProps 是否存在
+        if (!viewProps.value) {
+            error.message = '视图数据加载失败'
+            return
+        }
+        // 重置错误信息
+        error.message = ''
+        // 跳转至指定视图
+        if (props.todoId) return
+        router.replace({
+            name: 'tasks-project-main',
+            params: { viewType: viewProps.value.preference.viewType }
         })
-        .finally(() => (loading.value = false))
-})
+    },
+    { immediate: true }
+)
 </script>
 
 <template>
