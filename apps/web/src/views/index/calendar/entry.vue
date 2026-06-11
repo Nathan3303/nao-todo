ea95b74b30902548ab46c3d18a8e5a02afd24cd0
Nathan3303 <Lee1928@outlook.com>
2026-06-11 22:22:49 +0800

    fix(calendar): 修复 Calendar 页面在生产环境下的 “未实现” UI 布局错误

--- apps/web/src/views/index/calendar/entry.vue
+++ apps/web/src/views/index/calendar/entry.vue
@@ -4,6 +4,7 @@ import { AppAsideAdapter } from '@/layouts/app/'
 import { CalendarAside } from '@/layouts/calendar/aside'
 import { useCalendarView } from './calendar-view'
 import { env } from '@/infrastructure/constants/env'
+import { Loading as LoadingComp } from '@nao-todo/components'
 
 defineOptions({ name: 'CalendarView' })
 
@@ -14,7 +15,20 @@ onMounted(() => init())
 
 <template>
     <nue-container>
-        <nue-main>
+        <nue-main v-if="env.showUnimplementedFeatures">
+            <app-aside-adapter v-model:displayed="isDisplayAside" width="auto" min-width="unset" />
+            <nue-content fill style="overflow: hidden">
+                <nue-empty
+                    image-src="/images/feature.webp"
+                    image-size="8rem"
+                    description="日历页面还在规划中，敬请期待"
+                    style="height: 100%"
+                >
+                    <nue-button theme="small,primary" @click="$router.back()">返回</nue-button>
+                </nue-empty>
+            </nue-content>
+        </nue-main>
+        <nue-main v-else>
             <app-aside-adapter
                 v-model:displayed="isDisplayAside"
                 :width="asideWidth"
@@ -25,16 +39,21 @@ onMounted(() => init())
                 <calendar-aside />
             </app-aside-adapter>
             <nue-content fill style="overflow: hidden">
-                <router-view v-if="env.showUnimplementedFeatures" />
-                <nue-empty
-                    v-else
-                    image-src="/images/feature.webp"
-                    image-size="8rem"
-                    description="搜索页面还在规划中，敬请期待"
-                    style="height: 100%"
-                >
-                    <nue-button theme="small,primary" @click="$router.back()">返回</nue-button>
-                </nue-empty>
+                <router-view v-slot="{ Component }">
+                    <suspense>
+                        <component :is="Component" />
+                        <template #pending>
+                            <loading-comp height="100%" />
+                        </template>
+                        <template #fallback>
+                            <nue-empty image-src="/images/error.webp" image-size="6rem">
+                                <nue-text size="var(--nue-text-sm)">
+                                    加载失败, 请刷新页面重试
+                                </nue-text>
+                            </nue-empty>
+                        </template>
+                    </suspense>
+                </router-view>
             </nue-content>
         </nue-main>
     </nue-container>
